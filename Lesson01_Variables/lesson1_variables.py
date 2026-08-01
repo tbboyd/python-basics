568714f6c00c409ce7b953ccb6884f219e2e9aa3
Trystan Boyd <tbboyd7913@gmail.com>
2026-08-01 09:46:30 -0700

    Complete Lessons 1 and 2: Variables and Input

---
+++ Lesson01_Variables/lesson1_variables.py
@@ -0,0 +1,21 @@
+fav_food = 'sushi'
+fav_country = 'japan'
+fav_animal = 'black leapord'
+fav_movie = 'aquaman'
+dream_job = 'my own business'
+current_gpa = 2.479
+emergency_fund_goal = 2000
+savings = 0
+hours_worked_this_week = 18
+age_in_five_years = 24
+
+print(fav_food)
+print(fav_country)
+print(fav_animal)
+print(fav_movie)
+print(dream_job)
+print(age_in_five_years)
+print(hours_worked_this_week)
+print(emergency_fund_goal)
+print(current_gpa)
+print(savings)
---
+++ Lesson02_Input/lesson2_input.py
@@ -0,0 +1,7 @@
+favorite_food = input('What is your favorite food? ')
+dream_job = input('What is your dream job? ')
+favorite_country = input('What is your favorite country? ')
+
+print(favorite_food)
+print(favorite_country)
+print(dream_job)

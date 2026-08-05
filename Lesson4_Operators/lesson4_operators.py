2af260a93df757adc6b611dfbd5b0fdeb95c8739
Trystan Boyd <tbboyd7913@gmail.com>
2026-08-05 09:41:54 -0700

    Complete Lesson 4: Math Operations.

---
+++ Lesson4_Operators/lesson4_operators.py
@@ -0,0 +1,11 @@
+number1 = 15
+number2 = 4
+
+print('Addition: ', number1 + number2)
+print('Subtraction: ', number1 - number2)
+print('Multiplaction: ', number1 * number2)
+print('Division:', number1 / number2)
+print('Floor divide: ', number1 // number2)
+print('Remainder: ', number1 % number2)
+print('Exponent: ', number1 ** number2)
+
---
+++ Lesson4_Operators/paycheck_calculator.py
@@ -0,0 +1,15 @@
+hourly_rate = int(input('What is your hourly rate? '))
+weekly_hours_worked = int(input('How many hours have you worked this week? '))
+tax_perecentage = int(input('What perecentage of your income is taxed? '))
+
+gross_pay = hourly_rate * weekly_hours_worked
+taxes = gross_pay * (tax_perecentage / 100)
+net_pay = gross_pay - taxes
+
+print()
+print('==== Weekly Paycheck ====')
+print('Hourly rate: $', hourly_rate)
+print('Hours worked: ', weekly_hours_worked)
+print('Gross Pay: $', gross_pay)
+print('Taxes: $', taxes)
+print('Net Pay: $', net_pay)
